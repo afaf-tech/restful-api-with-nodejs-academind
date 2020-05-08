8fafe36fc457dc9ce8bc105c2328656b5548e321
afaf-tech <fatihalfikri5@gmail.com>
2020-05-09 00:44:49 +0700

    #0003

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const ProductRoutes = require('./api/routes/products');
+const OrderRoutes = require('./api/routes//orders');
+
+app.use('/products', ProductRoutes );
+app.use('/orders', OrderRoutes );
 app.use((req,res, next) => {
     res.status(200).json({
         message:"It works!"
